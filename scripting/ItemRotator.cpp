dbc55c56bd368964763fbcc3c07e8148de4eb3b6
Quentin LOPES <quentin.lopes@orange.fr>
2026-04-03 12:33:53 +0200

    FIX/ ItemRotator

--- scripting/ItemRotator.cpp
+++ scripting/ItemRotator.cpp
@@ -1,9 +1,4 @@
 #include "script_pch.h"
-#include "Systems/AnimatorSystem.h"
-
-#include <vector>
-#include <any>
-#include <iostream>
 
 #ifdef _WIN32
 #define SCRIPT_API __declspec(dllexport)
@@ -13,24 +8,16 @@
 
 class ItemRotator : public Engine::Scripting::NativeScript {
 public:
-    //==================== Public: functions ====================
+    float rotationSpeed = 180.0f;
+    float detectDistance = 0.01f;
+    Engine::ECS::Entity character = Engine::ECS::NULL_ENTITY;
 
     void OnInit() override {
         Inspect("Rotation Speed", &rotationSpeed);
         Inspect("Detect Distance", &detectDistance);
     }
 
-    void OnDestroy() override {
-        if (m_funcSys) {
-            m_funcSys->Unregister("Interact");
-        }
-    }
-
     void OnCreate() override {
-        // Get system
-        m_funcSys = engine->GetSystem<Engine::Systems::FunctionRegistrySystem>();
-
-        // Find character
         for (auto e : registry->View<Engine::Components::Transform>()) {
             if (registry->GetEntityName(e) == "Character") {
                 character = e;
@@ -39,25 +26,8 @@ public:
         }
 
         if (character == Engine::ECS::NULL_ENTITY) {
-            TerminalInstance->error("ItemRotator: Character not found!");
+            TerminalInstance->error("CoinRotator: Character not found!");
         }
-
-        // Validate system
-        if (!m_funcSys) {
-            std::cerr << "FunctionRegistrySystem not found!" << std::endl;
-            return;
-        }
-
-        // Register function
-        m_funcSys->Register("Interact", [this](std::vector<std::any> args) -> std::any {
-            if (args.size() == 1 && args[0].type() == typeid(Engine::ECS::Entity)) {
-                Engine::ECS::Entity entity = std::any_cast<Engine::ECS::Entity>(args[0]);
-                registry->DestroyEntity(entity);
-            } else {
-                std::cerr << "[Interact] Invalid type\n";
-            }
-            return {};
-        });
     }
 
     void OnUpdate(float dt) override {
@@ -81,31 +51,14 @@ public:
             start,
             end,
             entityID,
-            { true, detectDistance, {1,0,0}, {1,1,0}, {0,1,1}, {0.5f,0.5f,0.5f}, 0.05f, 0.012f }
+            { true, detectDistance, {1,0,0}, {1,1,0}, {0,1,1}, {0.5,0.5,0.5}, 0.05f, 0.012f }
         );
 
         if (hit.hitEntity == character) {
-            TerminalInstance->info("Item collected via Raycast!");
-            m_funcSys->Call("Interact", { entityID });
+            TerminalInstance->info("Coin collected via Raycast!");
+            registry->DestroyEntity(entityID);
         }
     }
-
-public:
-    //==================== Public: members ====================
-
-    float rotationSpeed = 180.0f;
-    float detectDistance = 0.01f;
-    Engine::ECS::Entity character = Engine::ECS::NULL_ENTITY;
-
-private:
-    //==================== Private: functions ====================
-
-    // None
-
-private:
-    //==================== Private: members ====================
-
-    Engine::Systems::FunctionRegistrySystem* m_funcSys = nullptr;
 };
 
 extern "C" SCRIPT_API Engine::Scripting::NativeScript* CreateScript() {
