37970852e8bd49a3927642323c6ca776dad40312
Quentin LOPES <quentin.lopes@orange.fr>
2026-04-08 09:37:18 +0200

    Fix Camera

--- scripting/PlateformNativeScript.cpp
+++ scripting/PlateformNativeScript.cpp
@@ -15,11 +15,92 @@ public:
     float val = 0.1f;
     float distancePatrol = 0.7f;
     float securityDistance = 0.69f;
-    float posY = 0.05f;
-    float posZ = 0.f;
+    float posY = 0.65f; // position de base en y
+    float posZ = -0.6f; // position de base en z
+    float posX = -0.325f; // position de base en x
 
     Engine::ECS::Entity targetPlateform = Engine::ECS::NULL_ENTITY;
     Engine::ECS::Entity targetCharacter = Engine::ECS::NULL_ENTITY;
+
+    void PlatformPattern(char AxePattern, Engine::ECS::Entity targetPl, Engine::ECS::Entity targetChar) {
+
+        // fais avec un switch et il faut mettre un char en mode x,y ou z et ca fais le pattern voulu pour la plateforme
+
+        auto& targetPlateformTransform = registry->GetComponent<Engine::Components::Transform>(targetPl);
+        auto& targetCharacterTransform = registry->GetComponent<Engine::Components::Transform>(targetChar);
+
+        switch (AxePattern) {
+        case 'x':
+        {
+            if (targetPlateformTransform.Position.x <= -distancePatrol) {
+                targetPlateformTransform.Position.x = -securityDistance;
+                invertPlateformDir = false;
+            }
+            if (targetPlateformTransform.Position.x >= distancePatrol) {
+                targetPlateformTransform.Position.x = securityDistance;
+                invertPlateformDir = true;
+            }
+
+            if (invertPlateformDir) {
+                targetPlateformTransform.Position.x -= speed * dt;
+                if (targetCharacterTransform.Position.x <= targetPlateformTransform.Position.x + val && targetCharacterTransform.Position.z <= targetPlateformTransform.Position.z + val &&
+                    targetCharacterTransform.Position.x >= targetPlateformTransform.Position.x - val && targetCharacterTransform.Position.z >= targetPlateformTransform.Position.z - val && targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + 0.1) {
+                    targetCharacterTransform.Position.x -= speed * dt;
+                }
+            }
+            if (!invertPlateformDir) {
+                targetPlateformTransform.Position.x += speed * dt;
+                if (targetCharacterTransform.Position.x <= targetPlateformTransform.Position.x + val && targetCharacterTransform.Position.z <= targetPlateformTransform.Position.z + val &&
+                    targetCharacterTransform.Position.x >= targetPlateformTransform.Position.x - val && targetCharacterTransform.Position.z >= targetPlateformTransform.Position.z - val && targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + 0.1) {
+                    targetCharacterTransform.Position.x += speed * dt;
+                }
+            }
+
+            targetPlateformTransform.Position.y = posY;
+            targetPlateformTransform.Position.z = posZ;
+            std::cout << "x\n";
+        }
+        break;
+        case 'y':
+        {
+            if (targetPlateformTransform.Position.y <= -distancePatrol) {
+                targetPlateformTransform.Position.y = -securityDistance;
+                invertPlateformDir = false;
+            }
+            if (targetPlateformTransform.Position.y >= distancePatrol) {
+                targetPlateformTransform.Position.y = securityDistance;
+                invertPlateformDir = true;
+            }
+
+            if (invertPlateformDir) {
+                targetPlateformTransform.Position.y -= speed * dt;
+                if (targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + val && targetCharacterTransform.Position.z <= targetPlateformTransform.Position.z + val &&
+                    targetCharacterTransform.Position.y >= targetPlateformTransform.Position.y - val && targetCharacterTransform.Position.z >= targetPlateformTransform.Position.z - val && targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + 0.1) {
+                    targetCharacterTransform.Position.y -= speed * dt;
+                }
+            }
+            if (!invertPlateformDir) {
+                targetPlateformTransform.Position.y += speed * dt;
+                if (targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + val && targetCharacterTransform.Position.z <= targetPlateformTransform.Position.z + val &&
+                    targetCharacterTransform.Position.y >= targetPlateformTransform.Position.y - val && targetCharacterTransform.Position.z >= targetPlateformTransform.Position.z - val && targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + 0.1) {
+                    targetCharacterTransform.Position.y += speed * dt;
+                }
+            }
+
+            targetPlateformTransform.Position.y = posY;
+            targetPlateformTransform.Position.z = posZ;
+            std::cout << "y\n";
+        }
+        break;
+        case 'z':
+        {
+
+        }
+        break;
+        default:
+            break;
+        }        
+    }
 
     void OnCreate() override {
         FindTarget();
@@ -50,37 +131,7 @@ public:
             if (targetPlateform == Engine::ECS::NULL_ENTITY) return;
         }
 
-        auto& targetPlateformTransform = registry->GetComponent<Engine::Components::Transform>(targetPlateform);
-        auto& targetCharacterTransform = registry->GetComponent<Engine::Components::Transform>(targetCharacter);
-
-        // Plateform Pattern 
-        if (targetPlateformTransform.Position.x <= -distancePatrol) {
-            targetPlateformTransform.Position.x = -securityDistance;
-            invertPlateformDir = false;
-        }
-        if (targetPlateformTransform.Position.x >= distancePatrol) {
-            targetPlateformTransform.Position.x = securityDistance;
-            invertPlateformDir = true;
-        }
-
-        // ici je fais le mouvement de la plateforme et j'ajoute le meme mouvement au perso que ca fasse genre que ca suit la plateforme mdr
-        if (invertPlateformDir) {
-            targetPlateformTransform.Position.x -= speed * dt;
-            if (targetCharacterTransform.Position.x <= targetPlateformTransform.Position.x + val && targetCharacterTransform.Position.z <= targetPlateformTransform.Position.z + val &&
-                targetCharacterTransform.Position.x >= targetPlateformTransform.Position.x - val && targetCharacterTransform.Position.z >= targetPlateformTransform.Position.z - val && targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + 0.1) {
-                targetCharacterTransform.Position.x -= speed * dt;
-            }
-        }
-        if (!invertPlateformDir) {
-            targetPlateformTransform.Position.x += speed * dt;
-            if (targetCharacterTransform.Position.x <= targetPlateformTransform.Position.x + val && targetCharacterTransform.Position.z <= targetPlateformTransform.Position.z + val &&
-                targetCharacterTransform.Position.x >= targetPlateformTransform.Position.x - val && targetCharacterTransform.Position.z >= targetPlateformTransform.Position.z - val && targetCharacterTransform.Position.y <= targetPlateformTransform.Position.y + 0.1) {
-                targetCharacterTransform.Position.x += speed * dt;
-            }
-        }
-
-        targetPlateformTransform.Position.y = posY;
-        targetPlateformTransform.Position.z = posZ;
+        PlatformPattern('x', targetPlateform, targetCharacter);
     }
 };
 
